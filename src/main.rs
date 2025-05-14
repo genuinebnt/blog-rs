2e01d50108aea21799aaa9524097a6cbd05796aa
genuinebasilnt <genuinebnt@gmail.com>
2025-05-14 10:22:05 +0530

    feat(app): running

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ async fn main() {
     let settings = blog_rs::config::Settings::new().expect("Failed to load settings");
     let connection_string =
         blog_rs::config::DatabaseSettings::connection_string_with_db(&settings.database);
+
     let state = AppState::build(connection_string).expect("Failed to build app state");
-    let app = create_app(state.clone());
-    let listener = TcpListener::bind("0.0,0.0:8080")
+    let app = create_app(state);
+
+    let address = format!(
+        "{}:{}",
+        settings.application.host, settings.application.port
+    );
+    let listener = TcpListener::bind(address)
         .await
         .expect("Failed to bind TCP listener");
+
     axum::serve(listener, app)
         .await
         .expect("Failed to start server");
